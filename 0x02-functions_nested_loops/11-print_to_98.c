771fb537df2d704da8d563e2634823feb9d2370f
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 14:58:41 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+
+void print_to_98(int n)
+{
+	for (n; n <= 98; n++)
+	{
+		printf(n);
+		printf(", ");
+	}
+	printf('\n');
+}
