f870b127cabd31d236590efa75050a4b4a2bca19
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 12:28:11 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i,j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; J < (97 + 26); j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
