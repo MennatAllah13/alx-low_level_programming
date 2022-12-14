40eb48cd049fc8657c386e4dfa5532bf5b876772
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 15:00:25 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Prints all natural numbers
@@ -11,7 +12,7 @@
 
 void print_to_98(int n)
 {
-	for (n; n <= 98; n++)
+	for (; n <= 98; n++)
 	{
 		printf(n);
 		printf(", ");
