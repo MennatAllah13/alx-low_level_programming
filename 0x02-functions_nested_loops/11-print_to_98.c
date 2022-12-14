a302ec0ec121eaaf3da0198b85027a513e6becdb
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 15:10:50 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -31,13 +31,13 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			if (n != 98)
-                        {
-                                printf("%d, ", n);
-                        }
-                        else
-                        {
-                                printf("%d\n", n);
-                        }
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
 		}
 	}
 	else
