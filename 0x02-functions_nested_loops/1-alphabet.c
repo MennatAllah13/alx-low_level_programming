9a2046241a966acebd16e79e4307ac558500dba1
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 12:19:59 -0800

    I sometimes suffer from insomnia. And when I can not fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	for (int i = 97; i < (97+26); i++)
+	{
+		_putchar(i);
+	}
+}
