f5a0510ccd060285da242fa80eae3ed63eb42027
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-14 14:14:02 -0800

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
+
+void times_table(void)
+{
+	int i, j, val;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			val = i * j;
+			_putchar(val);
+		}
+		_putchar('\n');
+	}	
+}
