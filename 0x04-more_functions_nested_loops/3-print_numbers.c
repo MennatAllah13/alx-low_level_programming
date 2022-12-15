29c4829a2abed6b53d37c0d75579fa433ae83507
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-15 09:44:08 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,11 @@
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
  */
-
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
