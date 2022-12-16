8a8a34ffccdfa4fe3e1bc79e6d1b9751fd07c832
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-15 22:09:53 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,28 @@ int main(void)
 {
 	int i = 1;
 	
-	while (i <= 100)
+	printf("%d", i);
+
+	while (i < 100)
 	{
+		i++;
+
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
-
-		i++;
 	}
 	printf("\n");
 	return (0);
