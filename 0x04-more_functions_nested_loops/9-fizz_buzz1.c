c8e8e48265d93a61476182cca988712bbb9b5195
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-18 10:51:20 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz1.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz1.c
@@ -4,31 +4,39 @@
  * main - prints Buzz each numbers of 3 and 5.
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int n;
-
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	int i = 1;
+	
+	while (i < 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i == 1)
+                {
+                    printf("%d", i);
+                }
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
 		else
 		{
-			printf(" %d", n);
+			printf(" %d", i);
 		}
+
+		i++;
+
 	}
+	
 	printf("\n");
+
 	return (0);
 }
