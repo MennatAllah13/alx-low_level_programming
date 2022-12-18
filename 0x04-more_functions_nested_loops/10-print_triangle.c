a4144f3d7857528d1bb1eb890d9f935097cf1fe2
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-18 13:34:24 -0800

    Trianles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,17 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
-	}
-	_putchar('\n');
+        for (i = 1; i <= size; i++)
+        {
+                for (j = 1; j < size - i + 1; j++)
+                {
+                        _putchar(' ');
+                }
+                for ( j = 1; j <= i; j++)
+                {
+                        _putchar('#');
+                }
+                _putchar('\n');
+        }
+        _putchar('\n');
 }
