2a460969110ce4ed6450abc13d9ae38b44f7c435
Mennat Allah <mennatallahhisham@gmail.com>
2022-12-21 10:24:16 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count1 = 0, count2 = 0;
+
+	while (dest[count1] != '\0')
+	{
+		count1++;
+	}
+	while 
+}
