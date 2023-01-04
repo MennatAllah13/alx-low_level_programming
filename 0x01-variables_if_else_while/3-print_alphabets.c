6cbf58045b5e6ad81aa6f4edeb6138aa0179a37a
Mennat Allah <mennatallahhisham@gmail.com>
2023-01-04 15:39:14 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* char alp[26] = "abcdefghijklmnopqrstuvwxyz";*/
+	int i;
+
+	for (i = 97; i < 97 + 26; i++)
+	{
+		_putchar(i);
+	}
+	for (i = 65; i < 65 + 26; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
