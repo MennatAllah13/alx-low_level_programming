3a4dbf98967454ee5d94cd0a4967b1562e7b1c97
Mennat Allah <mennatallahhisham@gmail.com>
2023-01-04 15:50:45 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i % 10 == 0)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return(0);
+}
