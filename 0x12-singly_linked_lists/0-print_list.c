ff7b7d3a8d797beba53239ed4b1954c61080ed73
Mennat Allah <mennatallahhisham@gmail.com>
2023-01-30 14:57:36 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,12 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t listed;
-	listed = 0;
+	size_t listed = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[%d] (nil)\n", 0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
