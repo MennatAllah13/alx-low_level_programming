203c7170c35a16121ba050f267d12453ad50f8bf
Mennat Allah <mennatallahhisham@gmail.com>
2023-01-30 15:01:53 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,16 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] (nil)\n", 0);
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		}
+
 		listed++;
+		h = h->next;
 	}
 	return (listed);
 }
