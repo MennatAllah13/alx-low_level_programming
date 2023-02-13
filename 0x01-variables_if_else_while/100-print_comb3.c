13579e650ea3de751dad2fe3138bbc05f6f2aafa
Mennat Allah <mennatallahhisham@gmail.com>
2023-02-13 11:10:59 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48, j = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			if (i != j)
+			{
+				if (i < j)
+				{
+					putchar(i);
+					putchar(j);
+					if (i == 56 && j == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;	
+		}
+		i++;
+	}
+}
