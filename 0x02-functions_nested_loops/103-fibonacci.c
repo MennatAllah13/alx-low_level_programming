9f99805e74c6916e702be6db28ea821879dcd92e
Mennat Allah <mennatallahhisham@gmail.com>
2023-02-13 14:42:29 -0800

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	long int x = 1, y = 2, temp, sum = 0;
@@ -10,8 +17,7 @@ int main(void)
 		x = y;
 		y = temp;
 		if (x % 2 == 0)
-			sum = sum + x;
-	
+			sum = sum + x;	
 	}
 	printf("%ld\n", sum);
 	return (0);
