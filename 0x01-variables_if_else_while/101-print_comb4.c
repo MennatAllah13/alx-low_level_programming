cf8633f83c2b1fa21d55d57d115c77480e126e3c
Mennat Allah <mennatallahhisham@gmail.com>
2023-02-13 11:38:56 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48, y, z;
+
+	while (x < 56)
+	{
+		y = 48;
+		while (y < 57)
+		{
+			z = 48;
+			while (z < 58)
+			{
+				if (x != y && x != z && y != z)
+				{
+					if (x < y && y < z)
+					{
+						putchar(x);
+						putchar(y);
+						putchar(z);
+						if (x == 55 && y == 56 && z == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
