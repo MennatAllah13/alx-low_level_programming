55f7c93a120488e99bc332478a457c74d34acc69
Mennat Allah <mennatallahhisham@gmail.com>
2023-02-14 13:09:22 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i])
+			dest[i] = src[i];
+		else
+			dest[i] = '\0'l
+	}
+	return (dest);
+}
