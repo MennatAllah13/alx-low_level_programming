d131d421705e74378e5f10984ae8d3a23df4b7e3
Mennat Allah <mennatallahhisham99@gmail.com>
2023-08-30 15:03:03 -0700

    Create 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _sqrt - returns square root of a number
+ * @x: test number
+ * @y: squared number
+ *
+ * Return: the square root of n
+ */
+int _sqrt(int x, int y)
+{
+        if (x * x == y)
+                return (x);
+        else if (x > y / 2)
+                return (-1);
+        return (_sqrt(x + 1, y));
+}
+
+/**
 * _sqrt_recursion - returns square root of a number
 * @n: number to be used
 *
@@ -11,20 +27,4 @@ int _sqrt_recursion(int n)
 	if (n <= 1)
 		return (n);
 	return (_sqrt(2, n));
-}
-
-/**
- * _sqrt - returns square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-int _sqrt(int x, int y)
-{
-	if (x * x == y)
-		return (x);
-	else if (x > y / 2)
-                return (-1);
-	return (_sqrt(x + 1, y));
 }
