2daa01cd09d660bb7f71846bef1cdfd4ab0e0f7d
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-05 05:08:42 -0700

    Add 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,9 @@
 
 /**
  * print_times_table - Prints the n times table
- *
  * @n: number times table (0 < n <= 15)
- *
  * Return: no return
  */
-
 void print_times_table(int n)
 {
 	int i, j, val;
@@ -20,34 +17,22 @@ void print_times_table(int n)
 			{
 				val = i * j;
 				if (val <= 9)
-				{
 					if (j != 0)
-					{
 						_putchar(' ');
 						_putchar(' ');
-					}
 					_putchar(val + 48);
-				}
 				else
-				{
 					if (val < 100)
-					{
 						_putchar(' ');
 						_putchar(val / 10 + 48);
 						_putchar(val % 10 + 48);
-					}
 					if (val > 99)
-					{
 						_putchar(((val / 100) % 10) + 48);
 						_putchar(((val / 10) % 10) + 48);
 						_putchar((val % 10) + 48);
-					}
-				}
 				if (j != n)
-				{
 					_putchar(',');
 					_putchar(' ');
-				}
 			}
 			_putchar('\n');
 		}
