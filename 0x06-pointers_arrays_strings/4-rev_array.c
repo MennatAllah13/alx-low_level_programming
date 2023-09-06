c25e275a0f86a42cba836c2a8f07ccd5e492f4a5
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-06 13:23:47 -0700

    Add 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,19 +7,16 @@
  * @n: number of elements of the array.
  * Return: no return.
  */
-
 void reverse_array(int *a, int n)
 {
-	int i, j = n, temp;
+	int i = 0, j = n, temp;
 
-	for (i = 0; i <= n / 2; i++)
+	while (i <= n / 2 && j >= n / 2)
 	{
-		if (j >= n / 2)
-		{
-			temp = a[j];
-			a[j] = a[i - 1];
-			a[i - 1] = temp;
-			j--;
-		}
+		temp = a[j];
+		a[j] = a[i - 1];
+		a[i - 1] = temp;
+		i++;
+		j--;
 	}
 }
