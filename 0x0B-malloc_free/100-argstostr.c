e3387c3196f90309e0df3b294824c318e1850faf
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-11 09:54:07 -0700

    Create README.md

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+char *argstostr(int ac, char **av)
+{
+	char *x;
+
+	if (ac == 0 || av == NULL)
+		return (0);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Low-level programming
+# 0x0D. C - Preprocessor
