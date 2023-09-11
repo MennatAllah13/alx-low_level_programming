5a46fb1c0e845be79d6b749101e06e8a98796cd8
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-11 14:27:20 -0700

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %s\n", d->age);
+		if (d->owner)
+                        printf("Owner: %s\n", d->owner);
+                else
+                        printf("Owner: (nil)\n");
+	}
+}
