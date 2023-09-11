d6c84ddef078e91b8a65a88f925ac3ac4f2880f0
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-11 13:13:13 -0700

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct that stores dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
