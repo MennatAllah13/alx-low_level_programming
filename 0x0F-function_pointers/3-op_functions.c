717ade72a52b060e9cdfea84800741cf961ff052
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-13 14:31:42 -0700

    Create 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,6 +4,7 @@
  * op_add - adds two numbers.
  * @a: first number.
  * @b: second number.
+ *
  * Return: addition.
  */
 int op_add(int a, int b)
@@ -15,6 +16,7 @@ int op_add(int a, int b)
  * op_sub - subtracts two numbers.
  * @a: first number.
  * @b: second number.
+ *
  * Return: subtraction.
  */
 int op_sub(int a, int b)
@@ -26,6 +28,7 @@ int op_sub(int a, int b)
  * op_mul - multiplies two numbers.
  * @a: first number.
  * @b: second number.
+ *
  * Return: multiplication.
  */
 int op_mul(int a, int b)
@@ -37,6 +40,7 @@ int op_mul(int a, int b)
  * op_div - divides two numbers.
  * @a: first number.
  * @b: second number.
+ *
  * Return: division.
  */
 int op_div(int a, int b);
@@ -53,6 +57,7 @@ int op_div(int a, int b);
  * op_mod - module of two numbers.
  * @a: first number.
  * @b: second number.
+ *
  * Return: division remainder.
  */
 int op_mod(int a, int b)
