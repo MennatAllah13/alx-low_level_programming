6ade52ba8b88c28ec26539fc4d006f9ac182e691
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-21 06:29:48 -0700

    Create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
-#include "lists.h"
+#include <stdio.h>
 
 /**
- * bmain - function to be executed before main
+ * bmain - function executed before main
  * Return: no return.
  */
 void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
