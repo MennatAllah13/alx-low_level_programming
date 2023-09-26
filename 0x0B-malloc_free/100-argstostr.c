c644dcf8695bf8d9c453e88c7bc7378c6cc161f7
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-26 14:44:31 -0700

    Create 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,8 +8,8 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *x;
-	int i, j = 0, c = 0;
+	char *ch;
+	int i, j = 0, c = 0, x;
 
 	if (ac == 0)
 		return (0);
@@ -24,25 +24,25 @@ char *argstostr(int ac, char **av)
 		}
 		c++;
 	}
-	x = malloc(sizeof(char) * (c + 1));
-	if (!x)
+	ch = malloc(sizeof(char) * (c + 1));
+	if (!ch)
 	{
-		free(x);
+		free(ch);
 		return (NULL);
 	}
 	i = 0;
-	for (j = x = 0; x < c; j++, x++)
+	for (j =  = 0; x < c; j++, x++)
 	{
 		if (av[i][j] == '\0')
 		{
-			c[x] = '\n';
+			ch[x] = '\n';
 			i++;
 			x++;
 			j = 0;
 		}
 		if (x < c - 1)
-			c[x] = av[i][j];
+			ch[x] = av[i][j];
 	}
-	c[x] = '\0';
-	return (c);
+	ch[x] = '\0';
+	return (ch);
 }
