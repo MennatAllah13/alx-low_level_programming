9365e3f3bb1287da2dfb9518b642aadd80b26591
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-26 12:36:40 -0700

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,7 +8,6 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  *
  */
@@ -18,6 +17,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+int _putchar(char c)
 size_t print_listint(const listint_t *h);
 
 #endif
