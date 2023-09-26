a4bbdd88db0f80614ad461ed6a9531ad9bea8d95
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-26 14:02:30 -0700

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
- * @h: head of a list.
- *
- * Return: numbers of nodes.
+ * print_listint - prints all the elements of a list_t list.
+ * @h: linked list of type int
+ * Return: size_t
  */
 size_t print_listint(const listint_t *h)
 {
@@ -16,5 +15,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
