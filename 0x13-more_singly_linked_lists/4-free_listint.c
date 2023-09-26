59fa823452440aa20333f21497bf2ef534cfac31
Mennat Allah <mennatallahhisham99@gmail.com>
2023-09-26 12:57:30 -0700

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list.
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head->next)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+	if (head)
+	{
+		free(head);
+	}
+}
