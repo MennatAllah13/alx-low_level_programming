8943613136caa36967e5563674a683c1815b5b44
Mennat Allah <mennatallahhisham99@gmail.com>
2023-10-02 15:07:46 -0700

    Create 3.cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,65 @@
 #include "main.h"
 
 /**
+ * closure - close file stream
+ * @file: file pointer
+ * Return: Void
+ */
+void closure(int file)
+{
+	int c = close(file);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
+
+/**
+ * cp - copy file from src to dis
+ * @src: source
+ * @dist: distenation
+ * Return: Always 0.
+ */
+void cp(char *src, char *dist)
+{
+	int f_src, f_dist, op = 1024;
+	char buffer[BUFFERSIZE];
+
+	f_src = open(src, O_RDONLY);
+	if (f_src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+	f_dist = open(dist, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
+	if (f_dist == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dist);
+		closure(f_src);
+		exit(99);
+	}
+	while (op == BUFFERSIZE)
+	{
+		op = read(f_src, buffer, BUFFERSIZE);
+		if (op == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+			exit(98);
+		}
+		op = write(f_dist, buffer, op);
+		if (op == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dist);
+			exit(99);
+		}
+	}
+	closure(f_dist);
+	closure(f_src);
+}
+
+/**
  * main - check the code
  * @argc: number of arg
  * @argv: array of value
@@ -16,66 +75,4 @@ int main(int argc, char *argv[])
 	}
 	cp(argv[1], argv[2]);
 	return (0);
-}
-
-/**
- * cp - copy file from src to dis
- * @source: source
- * @dist: distenation
- * Return: Always 0.
- */
-void cp(char *source, char *dist)
-{
-	int fd_src, fd_dist, op = 1024;
-	char buffer[BUFFERSIZE];
-
-	fd_src = open(source, O_RDONLY);
-	if (fd_src == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
-		exit(98);
-	}
-
-	fd_dist = open(dist, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
-	if (fd_dist == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dist);
-		close_file(fd_src);
-		exit(99);
-	}
-
-	while (op == BUFFERSIZE)
-	{
-		op = read(fd_src, buffer, BUFFERSIZE);
-		if (op == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
-			exit(98);
-		}
-		op = write(fd_dist, buffer, op);
-		if (op == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dist);
-			exit(99);
-		}
-	}
-
-	close_file(fd_dist);
-	close_file(fd_src);
-}
-
-/**
- * close_file - close file stream
- * @fd: file pointer
- * Return: Void
- */
-void close_file(int fd)
-{
-	int cls = close(fd);
-
-	if (cls == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
 }
